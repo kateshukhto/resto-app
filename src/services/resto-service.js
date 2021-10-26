2f58ac40c26d44d2b6d52a1f3e0827e0826fa73f
kateshukhto <ekaterinasuhto@gmail.com>
2021-10-26 19:01:05 +0300

    change package.json

--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -1,5 +1,5 @@
 export default class RestoService {
-  _baseUrl = 'http://localhost:3000';
+  _baseUrl = 'https://restoapp2021.herokuapp.com/api';
 
   async getMenuItems() {
     let response = await fetch(`${this._baseUrl}/menu`);
