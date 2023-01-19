0f67ed041f23aebc1609582ca5ab8ee3002361bb
spu68654 <ocf84360@jeoce.com>
2023-01-19 12:11:53 +0300

    change node version

--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -1,9 +1,9 @@
 export default class RestoService {
-  _baseUrl = 'https://restoapp2021.herokuapp.com/api';
+  _baseUrl = 'http://localhost:3000';
 
   async getMenuItems() {
     let response = await fetch(`${this._baseUrl}/menu`);
-    if(!response.ok) {
+    if (!response.ok) {
       throw new Error(`Could not fetch ${this._baseUrl}/menu`)
     }
     return await response.json()
